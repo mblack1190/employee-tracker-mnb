930b1dc4dc197cbb68293dd4f76cfc6c2531b84d
Meredith Black <mblack1190@gmail.com>
2020-05-24 20:12:03 -0500

    employee tracker application

---
+++ app.js
@@ -0,0 +1,93 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const teamMembers = [];
+
+function createManager() {
+    console.log("Please build your team");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is your manager's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is your manager's id?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your manager's email?",
+        validate: answer => {
+          const pass = answer.match(
+            /\S+@\S+\.\S+/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is your manager's office number?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      }
+    ]).then(answers => {
+      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+      teamMembers.push(manager);
+      createTeam();
+    });
+  }
+function createTeam() {
+    inquirer.prompt([
+    {
+        type: "list",
+        name: "setRole",
+        message: "What type of employee?",
+        choices: ["Engineer", "Intern", "Done"]
+      }]).then(function (userChoice){
+        switch(userChoice.setRole) {
+            case "Engineer":
+            addEngineer();
+              break;
+            case "Intern":
+              addIntern();
+              break;
+            default:
+            buildTeam();
+          }
+      })
+}
+  createManager();
