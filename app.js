4475038389d25773f13a402f3aa7d4c780ff7295
Meredith Black <mblack1190@gmail.com>
2020-05-28 17:51:52 -0500

    finished

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require("path");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const render = require("./lib/html-renderer");
+
+ const OUTPUT_DIR = path.resolve(__dirname, 'output')
+ const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
 const teamMembers = [];
 
@@ -86,8 +90,135 @@ function createTeam() {
               addIntern();
               break;
             default:
-            buildTeam();
+             buildTeam();
           }
       })
 }
+function addEngineer() {
+  console.log("Please build your team");
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "What is your employee's name?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+    },
+    {
+      type: "input",
+      name: "engineerId",
+      message: "What is your employee's id?",
+      validate: answer => {
+        const pass = answer.match(
+          /^[1-9]\d*$/
+        );
+        if (pass) {
+          return true;
+        }
+        return "Please enter a positive number greater than zero.";
+      }
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "What is your eployee's email?",
+      validate: answer => {
+        const pass = answer.match(
+          /\S+@\S+\.\S+/
+        );
+        if (pass) {
+          return true;
+        }
+        return "Please enter a valid email address.";
+      }
+    },
+    {
+      type: "input",
+      name: "githubName",
+      message: "What is your gitHub user name?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+    }
+  ]).then(answers => {
+    const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.githubName);
+    teamMembers.push(engineer);
+    createTeam();
+  
+  });
+}
+
+function addIntern() {
+  console.log("Please build your team");
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "employeeName",
+      message: "What is your employee's name?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "What is your employee's id?",
+      validate: answer => {
+        const pass = answer.match(
+          /^[1-9]\d*$/
+        );
+        if (pass) {
+          return true;
+        }
+        return "Please enter a positive number greater than zero.";
+      }
+    },
+    {
+      type: "input",
+      name: "employeeEmail",
+      message: "What is your employee's email?",
+      validate: answer => {
+        const pass = answer.match(
+          /\S+@\S+\.\S+/
+        );
+        if (pass) {
+          return true;
+        }
+        return "Please enter a valid email address.";
+      }
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What is your School?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        return "Please enter at least one character.";
+      }
+    }
+  ]).then(answers => {
+    const intern = new Intern(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.school);
+    teamMembers.push(intern);
+    createTeam();
+  });
+}
+function buildTeam() {
+  if(!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+  }
+  fs.writeFileSync(outputPath, render(teamMembers),  "utf-8")
+}
+
   createManager();
